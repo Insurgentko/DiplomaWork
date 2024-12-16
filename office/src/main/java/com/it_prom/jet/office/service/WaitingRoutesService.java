2fa9a8a13c779e633eeaa663b83da22675c56ba7
Insurgentko <123051300+Insurgentko@users.noreply.github.com>
2024-12-16 12:30:49 +0200

    created services to making paths

---
+++ office/src/main/java/com/it_prom/jet/office/service/WaitingRoutesService.java
@@ -0,0 +1,43 @@
+package com.it_prom.jet.office.service;
+
+import com.it_prom.jet.common.bean.Route;
+import com.it_prom.jet.common.bean.RoutePath;
+import com.it_prom.jet.common.bean.RoutePoint;
+import com.it_prom.jet.office.provider.AirPortsProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+@Service
+@RequiredArgsConstructor
+public class WaitingRoutesService {
+    private final AirPortsProvider airPortsProvider;
+    private final Lock lock = new ReentrantLock(true);
+    private final List<Route> list = new ArrayList<>();
+
+    public List<Route> list(){
+        return list;
+    }
+    public void add(Route route){
+        try {
+            lock.lock();
+            list.add(route);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void remove(Route route){
+        try {
+            lock.lock();
+            list.removeIf(route::equals);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+}
