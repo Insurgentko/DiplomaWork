267209d23994b741c0ac501d3706ce4a54958b38
Olexandr Zaitsev <canyakek@gmail.com>
2024-12-18 23:02:21 +0200

    generated nuxt-js ui by Node.js

---
+++ ui/pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div style="width: 100%; min-height: 100vh; display: inline-flex; ">
+    <div class="messages"></div>
+    <div class="radar"></div>
+  <div/>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
+
+<style>
+*{
+  margin: 0; padding: 0;
+}
+
+.messages{
+  padding: 10px;
+  width: 10%;
+  background: #232323;
+}
+
+.radar{
+  width: 90%;
+  position: relative;
+  background: #232323;
+}
+</style>
